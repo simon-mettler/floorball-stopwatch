8b35b9d45c2344f05f610d22c864b0aedb2aea7c
Simon Mettler <admin@simonmettler.com>
2020-08-26 10:21:25 +0200

    add pause player change

--- js/floorball-stopwatch.js
+++ js/floorball-stopwatch.js
@@ -19,12 +19,19 @@ let watchParts = {
 		z: 0,	x: 0, c: 0, v: 0, b: 0,
 		1: 0, 2: 0, teams: 0
 	},
-	globalToggle: {
+	pauseToggle: {
 		q: 0,	w: 0, e: 0, r: 0, t: 0,
 		y: 0,	u: 0, i: 0, o: 0, p: 0,
 		a: 0,	s: 0, d: 0, f: 0, g: 0,
 		z: 0,	x: 0, c: 0, v: 0, b: 0,
-		1: 0, 2: 0
+		1: 0, 2: 0, all: 0
+	},
+	pauseSwitch: {
+		q: 0,	w: 0, e: 0, r: 0, t: 0,
+		y: 0,	u: 0, i: 0, o: 0, p: 0,
+		a: 0,	s: 0, d: 0, f: 0, g: 0,
+		z: 0,	x: 0, c: 0, v: 0, b: 0,
+		1: 0, 2: 0, all: 0
 	},
 	watchCounter: {
 		q: 0,	w: 0, e: 0, r: 0, t: 0,
@@ -38,12 +45,19 @@ function assignHotkey(watch, hotkey) {
 
 	hotkeys(hotkey, function(event, handler){
 
+		if(watchParts.pauseToggle.all == 1) {
+			if(watchParts.pauseToggle[hotkey] == 0) {
+				holdWatch(window[watch], hotkey)
+			} else {
+				discardWatch(window[watch], hotkey)
+			}
+		} else {
 		if(watchParts.watchToggle[hotkey] == 0) {
 			startWatch(window[watch], hotkey)
 		} else {
 			stopWatch(window[watch], hotkey)
 		}
-
+		}
 	});
 
 }
@@ -59,12 +73,14 @@ function createWatch(watch, hotkey) {
 
 
 function startWatch(watch, hotkey) {
-
+	
 	if(watchParts.watchToggle[hotkey] == 0) {
+		console.log('normal start ' + hotkey);
 		event.preventDefault();
 		watch.start();
 		watchParts.watchToggle[hotkey] = 1;
 		document.getElementById('blink-' + hotkey).classList.add("blink");
+		document.getElementById('blink-' + hotkey).classList.remove('hold');
 	}
 
 }
@@ -73,6 +89,7 @@ function startWatch(watch, hotkey) {
 function stopWatch(watch, hotkey) {
 
 	if(watchParts.watchToggle[hotkey] == 1) {
+		console.log('normal stop ' + hotkey);
 		event.preventDefault();
 		watch.stop();
 		watchParts.watchToggle[hotkey] = 0;
@@ -83,26 +100,43 @@ function stopWatch(watch, hotkey) {
 
 }
 
+function holdWatch(watch, hotkey) {
+
+		console.log('hold ' + hotkey);
+		watchParts.pauseToggle[hotkey] = 1;
+		document.getElementById('c-' + hotkey).innerHTML = watchParts.watchCounter[hotkey];
+		document.getElementById('blink-' + hotkey).classList.add('hold');
+
+}
+
+
+function discardWatch(watch, hotkey) {
+
+		console.log('discard ' + hotkey);
+		watchParts.pauseToggle[hotkey] = 0;
+		watchParts.watchCounter[hotkey] += 1;
+		document.getElementById('c-' + hotkey).innerHTML = watchParts.watchCounter[hotkey];
+		document.getElementById('blink-' + hotkey).classList.remove('hold');
+}
 
 function pauseGlobal(watch, hotkey) {
-
-	if(watchParts.watchToggle[hotkey] == 1) {
-
+	if(watchParts.watchToggle[hotkey] == 1 && watchParts.pauseToggle.all == 1 ) {
+		console.log('------ xxx ------');
+		watchParts.watchToggle[hotkey] = 0;
 		event.preventDefault();
 		watch.stop();
-		watchParts.watchToggle[hotkey] = 0;
-		watchParts.globalToggle[hotkey] = 1;
-		watchParts.watchCounter[hotkey] += 1;
+		watchParts.pauseToggle[hotkey] = 1;
+		// watchParts.watchCounter[hotkey] += 1;
 		document.getElementById('c-' + hotkey).innerHTML = watchParts.watchCounter[hotkey];
 		document.getElementById('blink-' + hotkey).classList.remove('blink');
 		document.getElementById('blink-' + hotkey).classList.add('hold');
 
-	} else if(watchParts.globalToggle[hotkey] == 1) {
-
+	} else if(watchParts.pauseToggle[hotkey] == 1) {
+		console.log('------ XXX ------');
 		event.preventDefault();
 		watch.start();
 		watchParts.watchToggle[hotkey] = 1;
-		watchParts.globalToggle[hotkey] = 0;
+		watchParts.pauseToggle[hotkey] = 0;
 		document.getElementById('blink-' + hotkey).classList.add('blink');
 		document.getElementById('blink-' + hotkey).classList.remove('hold');
 	}
@@ -111,12 +145,20 @@ function pauseGlobal(watch, hotkey) {
 
 
 // Start/Stop Line h
-function  starth() {
+function starth() {
 	startWatch(watchQ, 'q');
 	startWatch(watchW, 'w');
 	startWatch(watchE, 'e');
 	startWatch(watchR, 'r');
 	startWatch(watchT, 't');
+} 
+
+function holdh() {
+	holdWatch(watchQ, 'q');
+	holdWatch(watchW, 'w');
+	holdWatch(watchE, 'e');
+	holdWatch(watchR, 'r');
+	holdWatch(watchT, 't');
 } 
 
 function stoph() {
@@ -127,12 +169,28 @@ function stoph() {
 	stopWatch(watchT, 't');
 }
 
+function discardh() {
+	discardWatch(watchQ, 'q');
+	discardWatch(watchW, 'w');
+	discardWatch(watchE, 'e');
+	discardWatch(watchR, 'r');
+	discardWatch(watchT, 't');
+}
+
 hotkeys('h', function(event, handler){
-	starth();
+		if(watchParts.pauseToggle.all == 1) {
+			holdh();
+		} else {
+			starth();
+		}
 });
 
 hotkeys('shift+h', function(event, handler){
-	stoph();
+		if(watchParts.pauseToggle.all == 1) {
+			discardh();
+		} else {
+			stoph();
+		}
 });
 
 
@@ -145,6 +203,14 @@ function startj() {
 	startWatch(watchP, 'p');
 }
 
+function holdj() {
+	holdWatch(watchY, 'y');
+	holdWatch(watchU, 'u');
+	holdWatch(watchI, 'i');
+	holdWatch(watchO, 'o');
+	holdWatch(watchP, 'p');
+}
+
 function stopj() {
 	stopWatch(watchY, 'y');
 	stopWatch(watchU, 'u');
@@ -153,12 +219,28 @@ function stopj() {
 	stopWatch(watchP, 'p');
 }
 
+function discardj() {
+	discardWatch(watchY, 'y');
+	discardWatch(watchU, 'u');
+	discardWatch(watchI, 'i');
+	discardWatch(watchO, 'o');
+	discardWatch(watchP, 'p');
+}
+
 hotkeys('j', function(event, handler){
-	startj();
+		if(watchParts.pauseToggle.all == 1) {
+			holdj();
+		} else {
+			startj();
+		}
 });
 
 hotkeys('shift+j', function(event, handler){
-	stopj();
+		if(watchParts.pauseToggle.all == 1) {
+			discardj();
+		} else {
+			stopj();
+		}
 });
 
 
@@ -171,6 +253,14 @@ function startk() {
 	startWatch(watchG, 'g');
 }
 
+function holdk() {
+	holdWatch(watchA, 'a');
+	holdWatch(watchS, 's');
+	holdWatch(watchD, 'd');
+	holdWatch(watchF, 'f');
+	holdWatch(watchG, 'g');
+}
+
 function stopk() {
 	stopWatch(watchA, 'a');
 	stopWatch(watchS, 's');
@@ -179,12 +269,28 @@ function stopk() {
 	stopWatch(watchG, 'g');
 }
 
+function discardk() {
+	discardWatch(watchA, 'a');
+	discardWatch(watchS, 's');
+	discardWatch(watchD, 'd');
+	discardWatch(watchF, 'f');
+	discardWatch(watchG, 'g');
+}
+
 hotkeys('k', function(event, handler){
-	startk();
+		if(watchParts.pauseToggle.all == 1) {
+			holdk();
+		} else {
+			startk();
+		}
 });
 
 hotkeys('shift+k', function(event, handler){
-	stopk();
+		if(watchParts.pauseToggle.all == 1) {
+			discardk();
+		} else {
+			stopk();
+		}
 });
 
 
@@ -197,6 +303,14 @@ function startl() {
 	startWatch(watchB, 'b');
 }
 
+function holdl() {
+	holdWatch(watchZ, 'z');
+	holdWatch(watchX, 'x');
+	holdWatch(watchC, 'c');
+	holdWatch(watchV, 'v');
+	holdWatch(watchB, 'b');
+}
+
 function stopl() {
 	stopWatch(watchZ, 'z');
 	stopWatch(watchX, 'x');
@@ -205,44 +319,68 @@ function stopl() {
 	stopWatch(watchB, 'b');
 }
 
+function discardl() {
+	discardWatch(watchZ, 'z');
+	discardWatch(watchX, 'x');
+	discardWatch(watchC, 'c');
+	discardWatch(watchV, 'v');
+	discardWatch(watchB, 'b');
+}
+
 hotkeys('l', function(event, handler){
-	startl();
+		if(watchParts.pauseToggle.all == 1) {
+			holdl();
+		} else {
+			startl();
+		}
 });
 
 hotkeys('shift+l', function(event, handler){
-	stopl();
+		if(watchParts.pauseToggle.all == 1) {
+			discardl();
+		} else {
+			stopl();
+		}
 });
 
 
 // Global pause
 function globalPause() {
 
-		pauseGlobal(watchQ, 'q');
-		pauseGlobal(watchW, 'w');
-		pauseGlobal(watchE, 'e');
-		pauseGlobal(watchR, 'r');
-		pauseGlobal(watchT, 't');
+	if(watchParts.pauseToggle.all == 0) {
+		console.log('------ pause started ------');
+		watchParts.pauseToggle.all = 1;
+	} else { 
+		console.log('------ pause stopped ------');
+		watchParts.pauseToggle.all = 0; 
+	}
 
-		pauseGlobal(watchY, 'y');
-		pauseGlobal(watchU, 'u');
-		pauseGlobal(watchI, 'i');
-		pauseGlobal(watchO, 'o');
-		pauseGlobal(watchP, 'p');
+	pauseGlobal(watchQ, 'q');
+	pauseGlobal(watchW, 'w');
+	pauseGlobal(watchE, 'e');
+	pauseGlobal(watchR, 'r');
+	pauseGlobal(watchT, 't');
 
-		pauseGlobal(watchA, 'a');
-		pauseGlobal(watchS, 's');
-		pauseGlobal(watchD, 'd');
-		pauseGlobal(watchF, 'f');
-		pauseGlobal(watchG, 'g');
+	pauseGlobal(watchY, 'y');
+	pauseGlobal(watchU, 'u');
+	pauseGlobal(watchI, 'i');
+	pauseGlobal(watchO, 'o');
+	pauseGlobal(watchP, 'p');
 
-		pauseGlobal(watchZ, 'z');
-		pauseGlobal(watchX, 'x');
-		pauseGlobal(watchC, 'c');
-		pauseGlobal(watchV, 'v');
-		pauseGlobal(watchB, 'b');
+	pauseGlobal(watchA, 'a');
+	pauseGlobal(watchS, 's');
+	pauseGlobal(watchD, 'd');
+	pauseGlobal(watchF, 'f');
+	pauseGlobal(watchG, 'g');
 
-		pauseGlobal(watch1, '1');
-		pauseGlobal(watch2, '2');
+	pauseGlobal(watchZ, 'z');
+	pauseGlobal(watchX, 'x');
+	pauseGlobal(watchC, 'c');
+	pauseGlobal(watchV, 'v');
+	pauseGlobal(watchB, 'b');
+
+	pauseGlobal(watch1, '1');
+	pauseGlobal(watch2, '2');
 
 }
 
@@ -325,6 +463,7 @@ createWatch('watch2', '2');
 
 function ballposs() {
 
+		if(watchParts.pauseToggle.all == 0) {
 	if(watchParts.watchToggle.teams == 0) {
 		event.preventDefault();
 		watch1.start();
@@ -348,7 +487,7 @@ function ballposs() {
 		document.getElementById('blink-1').classList.remove('blink');
 		document.getElementById('blink-2').classList.add('blink');
 	}
-
+		}
 }
 
 hotkeys('n', function(event, handler){
